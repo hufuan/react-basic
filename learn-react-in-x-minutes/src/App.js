11a515feaf2f8b66976c451f11f62892da768f5a
fuhu <fuan.hu@netscout.com>
2021-05-13 23:31:29 +0800

    add case

--- learn-react-in-x-minutes/src/App.js
+++ learn-react-in-x-minutes/src/App.js
@@ -1,13 +1,47 @@
-import React , {useState} from 'react';
+import React , {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4'
 
+const LOCAL_STORAGE_KEY='todoApp, todos'
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  useEffect(()=>{
+    const storeTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+
+    if (storeTodos) setTodos(storeTodos)
+  }, []
+
+  )
+
+  useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo=>todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+  
+  function handleAddTodo(e) {
+      const name = todoNameRef.current.value
+      if (name === '') return
+      setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name}]
+
+      })
+      todoNameRef.current.value = null
+  }
+
   return (
     <>
-    <TodoList todos={todos}/>
-    <input type="text" />
-    <button> Add TodoList</button>
+    <TodoList todos={todos} toggleTodo={toggleTodo}/>
+    <input ref={todoNameRef} type='text'/>
+    <button onClick={handleAddTodo}> 
+        Add Todo
+    </button>
     <button>Clear complete</button>
     </>
   )
--- learn-react-in-x-minutes/src/Todo.js
+++ learn-react-in-x-minutes/src/Todo.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 
-export default function Todo( {todo}) {
+export default function Todo( {todo, toggleTodo}) {
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+    }
     return (
         <div>
-            {todo}
+            <label>
+                <input type="checkbox" checked={todo.complete} onChange=
+                {handleTodoClick}/>
+            {todo.name}
+            </label>
         </div>
     )
 }
