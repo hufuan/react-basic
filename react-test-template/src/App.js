7ceec305237ad636465ef052efe131372c846b38
hufuan <fuan_707@163.com>
2021-11-28 17:31:55 +0800

    test

---
+++ react-test-template/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ProfilePage  from './components/ProfilePage'
+
+function App() {
+    return (
+      <div>
+        <div>Hello!</div>
+        <div><ProfilePage /></div>
+      </div>
+  );
+}
+
+export default App;
